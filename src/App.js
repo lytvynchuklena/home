11681015c1b69b21c5a743c1516c99c8c1179de6
lytvynchuklena <lena.melanchina@gmail.com>
2021-05-08 21:49:22 +0300

    master

--- src/App.js
+++ src/App.js
@@ -16,11 +16,4 @@ function App() {
 export default App;
 
 
-// <CharacterComponent description={'bart'}
-//                     image={'https://lh3.googleusercontent.com/proxy/PBtz4JDkSt8Fb_1Fih6_FAKQPzVg0ZW4uwIrV6wiadIPsfL4CN4ZoNKTOKbqqK4CeCbDxAtfR9fthcfIRnvpl4ShSNZO0PU'}/>
-// <CharacterComponent description={'liza'}
-//                     image={'https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png'}/>
-// <CharacterComponent description={'gomer'}
-//                     image={'http://assets.stickpng.com/thumbs/5eb95cc417f3c600044a2913.png'}/>
-// <CharacterComponent description={'marge'}
-//                     image={'https://static.wikia.nocookie.net/theultimatesimpsons/images/0/0f/Marge-Simpson-icon.png/revision/latest?cb=20180210061653'}/>
+}/>
